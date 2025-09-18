b185b6c1f279f3aa5ac05eb0c080ec35d5d3d4c5
AzDeveloper <azgamedeveloper@gmail.com>
2025-09-18 20:04:23 +0700

    [feat] added navigation progress

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import { NavigationProgress } from "@/components/navigation-progress";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+          <NavigationProgress />
           {children}
           <Toaster position="top-right" richColors closeButton />
         </ThemeProvider>
